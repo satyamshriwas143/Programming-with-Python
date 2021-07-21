62c7260fa8de0d261cca507116078b4c8bfbedc4
Satyam Shriwas <80064827+satyamshriwas143@users.noreply.github.com>
2021-07-21 15:16:01 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,37 @@
+
+o strings are sequence of characters inside "" (double quotes) or '' (single quotes)
+
+Examples:
+	# comment
+	"" # empty string
+	"cheese burger" # text string
+	'cheese burger'
+	"12345678" # number string
+	"!@#$%^&*()" # punctuation marks
+
+
+I CODE SO FAST # invalid
+
+# I CODE SO FAST
+
+'I CODE SO FAST' # valid
+
+"I CODE SO FAST" # valid
+
+"""I CODE SO FAST""" # doc string
+
+'''I CODE SO FAST''' # doc string
+
+"""I
+CODE
+SO
+FAST"""
+
+'''I
+CODE
+SO
+FAST'''
+
+'''Python is an interpreted,
+object-oriented, high-level 
+programming language.'''
